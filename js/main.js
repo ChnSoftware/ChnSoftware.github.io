10242a5856247b30644aae6af8a53e3b9b7ef5c7
Cihan <cihanturkess@gmail.com>
2020-11-28 14:27:46 +0300

    navbar menu button yapıldı

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Back to Top
+const backToTopBtn = document.querySelector(".back-top");
+
+window.addEventListener("scroll", () => {
+	if (window.pageYOffset > 500) {
+		backToTopBtn.style.opacity = "0.6";
+	} else {
+		backToTopBtn.style.opacity = "0";
+	}
+});
+
+backToTopBtn.addEventListener("click", () => {
+	window.scrollTo(0, 0);
+});
+
+// Navbar Menu Button
+const navbar = document.querySelector(".navbar");
+const closeMenu = document.querySelector(".close-menu");
+
+window.addEventListener("scroll", () => {
+	if (!closeMenu.classList.contains("active") && window.pageYOffset > 50) {
+		closeMenu.style.opacity = "0";
+	} else {
+		closeMenu.style.opacity = "1";
+	}
+});
+closeMenu.addEventListener("click", () => {
+	closeMenu.classList.toggle("active");
+	navbar.classList.toggle("active");
+});
